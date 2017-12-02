61d6f3395f76b8e1e3abdcce21f2e2042f92c9a2
mh188 <34172842+mh188@users.noreply.github.com>
2017-12-01 20:13:23 -0500

    1st screen formatting and code cleanup.

--- src/SECProjUI.java
+++ src/SECProjUI.java
@@ -1,114 +1,86 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.*;
 
 import javax.swing.*;
 
-/**
- * We now see how to make an area in which we can edit text.
- *
- * @author Matthew Hertz
- */
 public class SECProjUI extends JFrame implements ActionListener{
-    /**
-     * Create a new window in which to write the great American novel.
-     */
 
-    private JButton submitButton;
-    private JLabel groupCountLabel;
+	private JButton submitButton;
 
+	public SECProjUI() {
 
-    public SECProjUI() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-
-
-        // Create two sub-panels and add them to the main panel
-        JPanel subPanel1 = new JPanel();
-        subPanel1.setLayout(new FlowLayout());
-        panel.add(subPanel1);
-        //add(subPanel1, BorderLayout.NORTH);
-        // Question: what if we add the subpanel directly to the JFrame?
-
-        JPanel subPanel2 = new JPanel();
-        subPanel2.setLayout(new FlowLayout());
-        //add(subPanel2, BorderLayout.SOUTH);
-        // Question: what if we add the subpanel directly to the JFrame?
-
-        // Create the label and add it to the panel.
-        groupCountLabel = new JLabel("Enter the group count");
-        
-        JTextArea groupCountBox = new JTextArea(2, 10);
-        groupCountBox.setLineWrap(true);
-        groupCountBox.setWrapStyleWord(true);
-        //groupCountBox.setText("");
-        JLabel previousScoreQuestion = new JLabel("Did you enter previous scores or not?");
-        
-        JRadioButton yesButton = new JRadioButton("Yes");
-        //yesButton.setMnemonic(KeyEvent.VK_B);
-        //yesButton.setActionCommand(birdString);
-        yesButton.setSelected(false);
-
-        JRadioButton noButton = new JRadioButton("No");
-        //noButton.setMnemonic(KeyEvent.VK_C);
-        //noButton.setActionCommand(catString);
-        noButton.setSelected(false);
-  
-
-        //Group the radio buttons.
-        ButtonGroup radioBtnGroup = new ButtonGroup();
-        radioBtnGroup.add(yesButton);
-        radioBtnGroup.add(noButton);
-       
-
-        // Create & add the buttons
-        submitButton = new JButton("Submit");
-        submitButton.addActionListener(this);
-        
-        
-
-        // Add things to the various panels
-        subPanel1.add(groupCountLabel);
-        subPanel1.add(groupCountBox);
-        subPanel1.add(previousScoreQuestion);
-        subPanel1.add(yesButton);
-        subPanel1.add(noButton);
-        subPanel1.add(submitButton);
-        
-        panel.add(subPanel1);
-        panel.add(subPanel2);
-        //panel.add(label);
-        add(panel, BorderLayout.CENTER);
-
-    }
-
-    /**
-     * Run the example program that starts the day
-     *
-     * @param args Command-line arguments which we will ignore.
-     */
-    public static void main(String[] args) {
+		//Create a sub-panel and add them to the main panel.
+		JPanel subPanel = new JPanel();
+		panel.add(subPanel);
+		subPanel.setPreferredSize(new Dimension(350,300));
 
 
-        SECProjUI projUI = new SECProjUI();
-        projUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        projUI.setTitle("Self and peer evaluation");
-        projUI.pack();
-        projUI.setVisible(true);
-    }
+		// Create the label for entering group count.
+		JLabel groupCountLabel = new JLabel("Enter the group count");
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+		JTextField groupCountBox = new JTextField();
+		groupCountBox.setPreferredSize(new Dimension(60,20));
+		groupCountBox.setHorizontalAlignment(JTextField.CENTER);;
 
-        if (e.getSource() == submitButton) {
+		//Create another label for asking question for previously entered scores.        
+		JLabel previousScoreQuestion = new JLabel("Did you enter previous scores or not?");
 
-            //groupCountLabel.setText("You clicked submit!!");
-        	SecondScreen secondScreen = new SecondScreen();
-        	//setVisible(false);
-        	secondScreen.setVisible(true);
-        }
-    }
+		JRadioButton yesButton = new JRadioButton("Yes");
+		yesButton.setSelected(false);
+		JRadioButton noButton = new JRadioButton("No");
+		noButton.setSelected(false);
+
+		//Group the radio buttons.
+		ButtonGroup radioBtnGroup = new ButtonGroup();
+		radioBtnGroup.add(yesButton);
+		radioBtnGroup.add(noButton);
+
+		// Create & add the buttons
+		submitButton = new JButton("Submit");
+		submitButton.addActionListener(this);      
+
+		// Add things to the various panels
+		subPanel.add(Box.createRigidArea(new Dimension(10,100)));
+
+		subPanel.add(groupCountLabel);
+		subPanel.add(groupCountBox);
+		subPanel.add(previousScoreQuestion);
+		subPanel.add(Box.createRigidArea(new Dimension(110,0)));
+		subPanel.add(yesButton);
+		subPanel.add(noButton);
+		subPanel.add(Box.createRigidArea(new Dimension(100,50)));
+		subPanel.add(submitButton, BorderLayout.CENTER);       
+		
+		panel.add(subPanel);
+
+		add(panel, BorderLayout.CENTER);
+
+	}
+
+	public static void main(String[] args) {
+
+		SECProjUI projUI = new SECProjUI();
+		projUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		projUI.setTitle("Self and peer evaluation");
+		projUI.pack();
+		projUI.setVisible(true);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		if (e.getSource() == submitButton) {
+
+			//groupCountLabel.setText("You clicked submit!!");
+			SecondScreen secondScreen = new SecondScreen();
+			setVisible(false);
+			secondScreen.setVisible(true);
+		}
+	}
 }
 
